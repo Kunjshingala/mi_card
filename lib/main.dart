dbd15eb5a26c090cbe0d67dc5ddc2670ae45c5f2
kunj shingala <kunjshingala.p@gmail.com>
2022-11-20 16:28:35 +0530

    Columns and rows Practices.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        // appBar: AppBar(
+        //   backgroundColor: Colors.black,
+        //   title: Text('Demo'),
+        //   centerTitle: true,
+        // ),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.red,
+                child: Text('Container 1'),
+              ),
+              Container(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                    child: Text('Container 1'),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellowAccent,
+                    child: Text('Container 2'),
+                  )
+                ],
+              )),
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.blue,
+                child: Text('Container 3'),
+              )
+            ],
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   backgroundColor: Colors.black,
+        //   child: Icon(Icons.add),
+        // ),
       ),
-    ),
-  );
+    );
+  }
 }
